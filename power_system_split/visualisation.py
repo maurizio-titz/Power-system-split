f899629fe6a824ee62d07742011dbc181b5b529d
Franz Kaiser <f.kaiser@fz-juelich.de>
2021-04-29 18:10:57 +0200

    Added function for likelihood calculation

--- power_system_split/visualisation.py
+++ power_system_split/visualisation.py
@@ -9,14 +9,12 @@ import numpy as np
 import networkx as nx
 
 
-
-
 def get_split_adjacencies_from_rocof_solutions(solution_dict,nx_graph):
     """obtain the adjacency matrices for each split in the
     rocof solution dictionary (see evaluate_split_observables).
 
     NOTE: This only takes into account splits that have been
-    prefiltered according to the nodes criterion or the load criterion
+    prefiltered according to the 'nodes' criterion or the 'load' criterion
     passed to the evaluation function"""
 
     adjacencies = []
@@ -31,3 +29,44 @@ def get_split_adjacencies_from_rocof_solutions(solution_dict,nx_graph):
             adjacencies.append(A.astype('bool'))
 
     return adjacencies
+
+def calc_likelihood_failure(nx_graph,
+                            splitting_cascades,
+                            number_of_snapshots,
+                            solution_dict = {})
+    """Calculate the likelihood that a) a given edge causes a split
+    if it fails (primary likelihood) and b) the likelihood that a
+    given edge fails at some point during a cascade. The number of snapshots
+    is the number of points in time (i.e. snapshots) used in the pypsa network
+    simulation.
+
+    If solution dict is given which is the output of evaluate_split_observables,
+    only the cascades that were used in the solution dict are considered,
+    otherwise all cascades are used to calculate the likelihood."""
+
+    likelihood_primary   = {(u,v):0.0 for u,v in nx_graph.edges()}
+    likelihood_secondary = {(u,v):0.0 for u,v in nx_graph.edges()}
+    number_of_edges      = len(nx_graph.edges())
+
+    if not len(solution_dict):
+        print("""Did not pass solution_dict to determine which cascades
+              to use. Evaluating all cascades.""")
+        for key in splitting_cascades.keys():
+            for current_cascade in splitting_cascades[key]:
+                for failed_edge in current_cascade:
+                    likelihood_secondary[failed_edge] += 1/(number_of_snapshots
+                                                            * number_of_edges)
+
+                likelihood_primary[current_cascade[0]] += 1/number_of_snapshots
+    else:
+
+        for key in solution_dict.keys():
+            for split_number in solution_dict[key][::2]:
+                current_cascade = splitting_cascades[key][split_number]
+                for failed_edge in current_cascade:
+                    likelihood_secondary[failed_edge] += 1/(number_of_snapshots
+                                                            * number_of_edges)
+
+                likelihood_primary[current_cascade[0]] += 1/number_of_snapshots
+
+    return likelihood_primary,likelihood_secondary
