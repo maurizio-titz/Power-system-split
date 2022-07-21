(fix) calculation of possible failures

power_system_split/cascade_sim.py
@@ -9,7 +9,7 @@ from numpy import dtype
 from scipy import sparse
 from scipy import linalg as sc_linalg
 from scipy.sparse.csgraph import connected_components
-
+import itertools
 
 def calc_num_parallel_after_failure(num_parallel):
     
@@ -30,6 +30,25 @@ def calc_num_parallel_after_failure(num_parallel):
 
         
     return num_parallel_new
+
+def calc_possible_double_line_failures(num_parallel_ls):
+    
+    
+    possible_failures = []
+    
+    # Add failures on two different links
+    possible_failures += list(itertools.combinations(range(3),2))
+    
+    # Add common mode failures (two circuits failing in one link)
+    for i in range(len(num_parallel_ls)):
+        num_parallel_one_fail = calc_num_parallel_after_failure(num_parallel_ls[i])
+        
+        # Only add common mode failure if more than one circuit is present
+        # (i.e., first failure did not remove all circuits)
+        if not np.isclose(num_parallel_one_fail,0):
+            possible_failures += [(i,i)]
+        
+    return possible_failures
 
 
 def remove_line_from_Bd(B_d_in, num_parallel_ls, line_limits_ls, del_idx, atol=1e-8):

scripts/2_run_cascade_code.py
@@ -9,7 +9,7 @@ import pandas as pd
 from scipy.sparse import spdiags
 
 sys.path.append('./')
-from power_system_split import utils
+from power_system_split import utils, cascade_sim
 
 # Setup paths to solved PyPSA networks and results of this script
 path_to_pypsa_network   = './data/European_networks_sclopf/'
@@ -61,23 +61,23 @@ indices = nx.get_edge_attributes(G, 'line_index')
 # Extract time steps
 current_snapshots = network.snapshots[:1]
 
-# Calculate possible double line failure (using non-bridges)
-possible_failures = utils.calc_possible_double_line_failures(G)
-
 # Build incidence matrix and susceptance matrix
 I_m = utils.construct_incidencematrix_from_orientation(G,return_np_array = False) 
 B_d = spdiags(np.array([attribs['weight'] for u,v, attribs in G.edges(data=True)]),
-              0,G.number_of_edges,G.number_of_edges)
+              0,G.number_of_edges(),G.number_of_edges())
 
 # Get array of num_parallels and of line limits
 num_parallel_list = np.array([attribs['num_parallel'] for u,v, attribs in G.edges(data=True)])
 line_limit_list = np.array([attribs['s_nom'] for u,v, attribs in G.edges(data=True)])
 
+# Calculate possible double line failure (using non-bridges)
+possible_failures = cascade_sim.calc_possible_double_line_failures(num_parallel_list)
+
 
 splitting_cascades = {}
 
 # Iterate over each time step in data set
-for snapshot in current_snapshots:
+for snapshot in current_snapshots[:1]:
 
     splitting_cascades[snapshot.strftime('%d_%m_%Y_%H')] = []
 
@@ -92,8 +92,8 @@ for snapshot in current_snapshots:
     # Simulate cascade for every tuple of trigger links
     for initial_failure in tqdm(possible_failures):
 
-        failing_links, system_split = utils.simulate_cascade_matrix_based(I_m, B_d, P_0, line_limit_list,
-                                                                          num_parallel_list, initial_failure)
+        failing_links, system_split = cascade_sim.simulate_cascade_matrix_based(I_m, B_d, P_0, line_limit_list,
+                                                                                num_parallel_list, initial_failure)
         if system_split:
             splitting_cascades[snapshot.strftime('%d_%m_%Y_%H')].append(failing_links)
 