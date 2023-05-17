fd58ea2525cf0f710800a527282b54ae14653154
Johannes Kruse <jo.kruse@fz-juelich.de>
2023-05-17 11:22:31 +0200

    refactor: introduce functions for data handling

--- scripts/2_run_cascade_code.py
+++ scripts/2_run_cascade_code.py
@@ -1,111 +1,73 @@
 import pickle
 import sys
-
 import networkx as nx
-import numpy as np
-import pypsa
 from tqdm import tqdm
-import pandas as pd
-from scipy.sparse import spdiags
 
-sys.path.append('./')
-from power_system_split import utils, cascade_sim
+sys.path.append('./') 
+from power_system_split import cascade_simulation, data_handling
 
 # Setup paths to solved PyPSA networks and results of this script
-path_to_pypsa_network   = './data/European_networks_sclopf/'
 save_path =  './results/sclopf/cascade_results/'
 
-# Load co2 level
-co2l = float(sys.argv[1])
-
-# Load PyPSA network
-network = pypsa.Network()
-network.import_from_netcdf(path_to_pypsa_network+f'sclopf-elec_s_800_ec_lv1.0_Co2L{co2l}-3H.nc')
-
-# The following line is needed to remove the outage lines used for SCLOPF. When not removed, multiple edges 
-# are added to the network graph
-outage_lines = network.lines[network.lines.index.str[-6:]=='outage'].index
-network.mremove('Line', outage_lines)
-network.determine_network_topology()
-
-
-######
-#TODO: add function for this. Adding time stamps is necessary because they are missing in the pypsa networks
-network.snapshots = pd.date_range(start='2013', freq='3H', periods=len(network.snapshots))
-
-network.snapshot_weightings.index = pd.date_range(start='2013', freq='3H', periods=len(network.snapshots))
-
-for component in network.all_components:
-    pnl = network.pnl(component)
-    attrs = network.components[component]["attrs"]
-
-    for k,default in attrs.default[attrs.varying].iteritems():
-        pnl[k].index = pd.date_range(start='2013', freq='3H', periods=len(network.snapshots))
-######
-
-
-
 # Select a particular subnetwork for calculations (if the pypsa network has different ones).
-# Otherwise you can choose None or -1.
 # For our data set, "0" indicates the Continental European AC grid. 
 snet_index = 0
 
-# Build graph for subnetwork
-G = utils.build_networkx_graph(network, snet_index= snet_index)
+# Load co2 level
+co2l = 0.0 #float(sys.argv[1]) #TODO include sys arg again!
 
-# Get line index 
-# Line index is a unique number to identify the given edge.
-# It is directly extracted from pypsa
-indices = nx.get_edge_attributes(G, 'line_index')
+# Load PyPSA network, the graph of the subnetwork and its matrices
+network = data_handling.load_pypsa_network(co2l)
+nx_graph = data_handling.build_networkx_graph(network, snet_index= snet_index)
+I_m, B_d, num_parallels, line_limits = data_handling.get_matrices_from_nx_graph(nx_graph)
 
-# Extract time steps
-current_snapshots = network.snapshots
-
-# Build incidence matrix and susceptance matrix
-I_m = utils.construct_incidencematrix_from_orientation(G,return_np_array = False) 
-B_d = spdiags(np.array([attribs['weight'] for u,v, attribs in G.edges(data=True)]),
-              0,G.number_of_edges(),G.number_of_edges()).asformat('csr')
-
-# Get array of num_parallels and of line limits
-num_parallel_list = np.array([attribs['num_parallel'] for u,v, attribs in G.edges(data=True)])
-line_limit_list = np.array([attribs['s_nom'] for u,v, attribs in G.edges(data=True)])
-
-# Calculate possible double line failure (using non-bridges)
-bridges = list(nx.bridges(nx.Graph(G)))
-link_indices = dict(zip(G.edges(), range(G.number_of_edges())))
-bridges_indices = [link_indices[link_name] for link_name in bridges]
-possible_failures = cascade_sim.calc_possible_double_line_failures(num_parallel_list,
-                                                                   bridges_indices)
-
+# Calculate possible N-1 and N-2 failures (using non-bridges)
+bridges_indices = data_handling.nx_edges_to_matrix_indices(nx.bridges(nx_graph),
+                                                           nx_graph)
+n_2_failures = cascade_simulation.calc_possible_double_line_failures(num_parallels, bridges_indices)
+n_1_failures = cascade_simulation.calc_possible_single_line_failures(num_parallels, bridges_indices)
 
 splitting_cascades = {}
 
-# Iterate over each time step in data set
+#TODO: remove this when using all snapshots
+current_snapshots = network.snapshots
+
+#TODO: one cascade simulation (one trigger) is 2x faster on my laptop than on Otter. Why? 
+
+### Check N-1 stability ###
+
 for snapshot in current_snapshots:
 
-    print('#### Co2 level', co2l, ' snapshot ', snapshot.strftime('%d_%m_%Y_%H'), ' ####')
+    print('#### N-1 failures: Co2 level', co2l, ' snapshot ', snapshot.strftime('%Y-%m-%d %H:00'), ' ####')
+
+    P_0 = data_handling.get_effective_injections(network, snapshot, nx_graph)
+
+    for initial_failure in tqdm(n_1_failures):
+
+        failing_links, system_split = cascade_simulation.simulate_cascade(I_m, B_d, P_0, line_limits,
+                                                                          num_parallels, initial_failure,
+                                                                          max_cascade_length=1)
+        if len(failing_links)>1:
+            raise(RuntimeError('PyPSA networks are not N-1 stable!'))
+
+
+### Simulation of N-2 failures ###
+
+for snapshot in current_snapshots:
+
+    print('#### N-2 failures: Co2 level', co2l, ' snapshot ', snapshot.strftime('%Y-%m-%d %H:00'), ' ####')
     
-    splitting_cascades[snapshot.strftime('%d_%m_%Y_%H')] = []
+    splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')] = {}
 
-    initial_loading = {e: np.sum([network.lines_t.p0.loc[snapshot].loc[index] for index in index_list]) for
-                       e, index_list in indices.items()} # index_list contains only one line if G is not a multigraph 
-                                                         # (we use a simple graph at the moment)
-    P_0 = utils.get_effective_injections(G, initial_loading)
 
-    #TODO: do we still need the following three line?
-    l_copy = initial_loading.copy()
-    for key in l_copy.keys():
-        initial_loading[key[::-1]] = initial_loading[key]
+    P_0 = data_handling.get_effective_injections(network, snapshot, nx_graph)
 
-    # Simulate cascade for every tuple of trigger links
-    for initial_failure in tqdm(possible_failures):
+    for initial_failure in tqdm(n_2_failures):
 
-        failing_links, system_split = cascade_sim.simulate_cascade(I_m, B_d, P_0, line_limit_list,
-                                                                   num_parallel_list, initial_failure,
-                                                                   epsilon=0.1)
+        failing_links, system_split = cascade_simulation.simulate_cascade(I_m, B_d, P_0, line_limits,
+                                                                          num_parallels, initial_failure)
         if system_split:
-            splitting_cascades[snapshot.strftime('%d_%m_%Y_%H')].append(failing_links)
+            splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')][tuple(initial_failure)] = failing_links 
 
-    # Save the current results after each snapshot and replace old version
     with open(save_path + f'system_splits_Co2L{co2l}.pickle' , 'wb') as handle:
         pickle.dump(splitting_cascades, handle, protocol = pickle.HIGHEST_PROTOCOL)
