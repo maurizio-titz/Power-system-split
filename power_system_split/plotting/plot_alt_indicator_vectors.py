743802bc440662a3b7836a1783a4a44b6d5b6014
p.boettcher <p.boettcher@fz-juelich.de>
2023-09-09 00:10:58 +0200

    (add) skeleton of plotting script

---
+++ power_system_split/plotting/plot_alt_indicator_vectors.py
@@ -0,0 +1,32 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Plot details on the indicators values"""
+
+import geopandas
+
+from matplotlib import pyplot as plt
+
+
+def plot_initial_trigger_likelihood():
+    
+    return
+
+
+def plot_secondary_failure_likelihood():
+    
+    return
+
+
+def plot_histogram_failed_links():
+
+    return
+
+
+def plot_failure_edge_failure_map():
+    
+    return
+
+def plot_rocof_failure_map():
+    
+    return
