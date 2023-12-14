5c26c5e0938a1149fdf34778bc61d01f09e8be6b
p.boettcher <p.boettcher@fz-juelich.de>
2023-12-14 17:24:50 +0100

    (add) writing out nested dictionary only at the end

--- scripts/run_cascade_code.py
+++ scripts/run_cascade_code.py
@@ -76,22 +76,28 @@ def run_cascade_dual_line_failures(co2l: float, n_nodes: int,
     print('\n#### N-2 failures: Co2 level', co2l, ' | #Nodes:', n_nodes, ' ####')
     for idx_snap, snapshot in enumerate(tqdm(network.snapshots)):
         key_now = snapshot.strftime('%Y-%m-%d %H:00')
-        splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')] = {}
+        #splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')] = {}
 
         P_0 = data_handling.get_effective_injections(network, snapshot, nx_graph)
 
         for initial_failure in tqdm(n_2_failures, leave=False):
-
+            res_dict = dict()
             failing_links, system_split = cascade_simulation.simulate_cascade(I_m, B_d, P_0, line_limits,
                                                                               num_parallels, initial_failure)
             
+            
+            
             if save_all_cascades:
-                splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')][tuple(initial_failure)] = failing_links
+                #splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')][tuple(initial_failure)] = failing_links
+                res_dict[tuple(initial_failure)] = failing_links, system_split
             
             elif system_split:
-                splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')][tuple(initial_failure)] = failing_links 
+                #splitting_cascades[snapshot.strftime('%Y-%m-%d %H:00')][tuple(initial_failure)] = failing_links 
+                res_dict[tuple(initial_failure)] = failing_links
+                
+        splitting_cascades[key_now] = res_dict
         
-        fpath_out = save_path + f"system_splits_Co2L{co2l}_n{n_nodes}"
+    fpath_out = save_path + f"system_splits_Co2L{co2l}_n{n_nodes}"
     
     if save_all_cascades:
             fpath_out += "_allcascades"
