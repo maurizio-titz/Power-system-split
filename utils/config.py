2b10aaa6b1ecbb11d0bac1e1dad4338e7ad7fd29
p.boettcher <p.boettcher@fz-juelich.de>
2024-01-04 15:41:17 +0100

    minor

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 root_path = '../' # This defaults to './', which should be the repository path
 data_path = "/media/data/system_split/data/"
 results_path = "/media/data/system_split/results/"
