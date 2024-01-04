89ebf7ce05064a5c2e457ab8112ebf4ade6b8e11
p.boettcher <p.boettcher@fz-juelich.de>
2024-01-04 15:40:36 +0100

    (add) added config of mattermost url

--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,5 @@
 root_path = '../' # This defaults to './', which should be the repository path
 data_path = "/media/data/system_split/data/"
 results_path = "/media/data/system_split/results/"
+
+mattermost_url = None
