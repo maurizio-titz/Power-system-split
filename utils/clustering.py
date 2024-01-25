9bcfd09bfca4b5bb87448d5ae010c512b99233e1
m.titz <m.titz@fz-juelich.de>
2024-01-25 17:10:00 +0100

    clean up/refactor

---
+++ utils/clustering.py
@@ -0,0 +1,191 @@
+import numpy as np
+from sklearn.cluster import KMeans
+import pickle
+import gzip
+from sklearn.metrics import silhouette_score
+from tqdm import tqdm
+import sys
+from utils.indicator_utils import *
+
+sys.path.append("./")
+
+
+def cluster_kmeans(
+    n_nodes,
+    co2l,
+    n_clusters,
+    indicator_type,
+    path_to_indicator_vectors,
+    path_to_clustering_results,
+    transformation=None,
+    mask=None,
+    weights=None
+):
+    """generates clusters with k-means algorithm and saves the results
+
+    Args:
+        n_nodes (int): number of nodes in the network
+        co2l (float): co2 level
+        n_clusters (int): number of clusters
+        indicator_type (string): which type of indicator vector to use
+        path_to_indicator_vectors (string or ): ....
+        path_to_clustering_results (string): ....
+    """
+
+    if weights is None:
+        indicator_vectors = load_indicator_vectors(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask, weights=None)
+    else:
+        indicator_vectors, weights = load_indicator_vectors(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask, weights=weights)
+
+    with gzip.open(f"{path_to_clustering_results}/weights.pkl","wb") as fh_out:
+        pickle.dump(weights, fh_out)
+    
+    # transform indicator vector
+    indicator_vectors = transform_indicator_vectors(indicator_vectors, transformation)
+    
+    # run KMeans
+    kmeans = KMeans(n_clusters=n_clusters)
+    kmeans.fit(indicator_vectors, sample_weight=weights)
+
+    labels = kmeans.labels_
+    centroids = kmeans.cluster_centers_
+    inertia = kmeans.inertia_
+    silhouette_avg = silhouette_score(indicator_vectors, labels, sample_size=10000)
+    samples_per_cluster = calculate_samples_per_cluster(n_clusters, weights, labels)
+
+    # calculate the mean distance to the centroid for each cluster
+    centroid_mean_distance = calculate_mean_distances_to_centroid(n_clusters, indicator_vectors, labels, centroids, centroid_mean_distance)
+
+    if transformation != None:
+        transformation_string = "_" + transformation
+    else:
+        transformation_string = ""
+
+    with gzip.open(
+        f"{path_to_clustering_results}/{indicator_type}{transformation_string}_Co2l{co2l}_n{n_nodes}_kmeans{n_clusters}.pkl",
+        "wb",
+    ) as fh_out:
+        pickle.dump(
+            [
+                labels,
+                centroids,
+                samples_per_cluster,
+                centroid_mean_distance,
+                inertia,
+                silhouette_avg,
+      #          weights,
+            ],
+            fh_out,
+        )
+
+    def transform_indicator_vectors(indicator_vectors, transformation):
+        if transformation == "sign":
+            indicator_vectors = np.sign(indicator_vectors)
+        elif transformation == "tanh":
+            indicator_vectors = np.tanh(indicator_vectors)
+        elif transformation == "clipped_tanh":
+            indicator_vectors = np.tanh(indicator_vectors)
+            indicator_vectors = np.clip(indicator_vectors, None, 0)
+        elif transformation == "clipped":
+            indicator_vectors = np.clip(indicator_vectors, None, 0)
+        elif transformation == "main_comp_max_lshare":
+            if indicator_type != "lshare":
+                raise ValueError(
+                    "is_main_component transformation only works for lshare indicator"
+                )
+            indicator_vectors = calc_is_main_component_indicator_vectors(indicator_vectors)
+        elif transformation == "main_comp_most_frequent_lshare":
+            if indicator_type != "lshare":
+                raise ValueError(
+                    "is_main_component transformation only works for lshare indicator"
+                )
+            indicator_vectors = calc_is_main_component_indicator_vectors(indicator_vectors, main_component_by="main_comp_most_frequent_lshare")
+        elif transformation == "not_zero":
+            indicator_vectors = np.array(indicator_vectors != 0, dtype=int)
+        else:
+            raise ValueError(f"transformation {transformation} not implemented")
+        
+        return indicator_vectors
+
+def calculate_samples_per_cluster(n_clusters, weights, labels):
+    if weights is None:
+        samples_per_centroid = np.unique(labels, return_counts=True)[1]
+    else:
+        samples_per_centroid = [(labels==i)*weights for i in n_clusters]
+    return samples_per_centroid
+
+def calculate_mean_distances_to_centroid(n_clusters, indicator_vectors, labels, centroids, centroid_mean_distance):
+    centroid_mean_distance = []
+    for i_centroid in range(n_clusters):
+        mean_distance = mean_distance_to_centroid(
+            indicator_vectors, centroids, i_centroid, labels
+        )
+        centroid_mean_distance.append(mean_distance)
+    return centroid_mean_distance
+
+def mean_distance_to_centroid(indicator_vectors, centroids, i_centroid, cluster_labels):
+    """Calculate Euclidean distance for each data point assigned to centroid
+
+    Args:
+        indicator_vectors (array): indicator
+        i_centroid (_type_): the centroide of interest
+        cluster_labels (array): cluster labels of indicator
+
+    Returns:
+        float: mean distance of all data points assigned to the cluster from the centroid
+    """
+    centroid = centroids[i_centroid]
+    assert (
+        indicator_vectors.shape[1] == centroid.shape[0]
+    ), f"indicator_vectors.shape[1] != centroid.shape[0], {indicator_vectors.shape[1]} != {centroid.shape[0]}"
+
+    distances = [
+        np.linalg.norm(indicator_vector - centroid)
+        for indicator_vector in indicator_vectors[cluster_labels == i_centroid]
+    ]
+    assert np.isnan(distances).any() == False, "NaN in distances"
+
+    return np.mean(distances)
+
+
+def load_clustering(
+    n_nodes, co2l, n_clusters, indicator_type, path_to_clustering_results, transformation=None
+):
+    """load clustering results from zip pickle file
+
+    Args:
+        n_nodes (int): _description_
+        co2l (listlike or float): _description_
+        n_clusters (_type_): number of cluster in kmeans
+        indicator_type (string): type of indicator vector
+        path_to_clustering_results (_type_): _description_
+
+    Returns:
+        (tuple of arrays): labels, centroids, inertia, silhouette_avg
+    """
+
+    if transformation != None:
+        transformation_string = "_" + transformation
+    else:
+        transformation_string = ""
+
+    with gzip.open(
+        f"{path_to_clustering_results}/{indicator_type}{transformation_string}_Co2l{co2l}_n{n_nodes}_kmeans{n_clusters}.pkl",
+        "rb",
+    ) as fh_out:
+        (
+            labels,
+            centroids,
+            samples_per_centroid,
+            centroid_mean_distance,
+            inertia,
+            silhouette_avg,
+        ) = pickle.load(fh_out)
+    return (
+        labels,
+        centroids,
+        samples_per_centroid,
+        centroid_mean_distance,
+        inertia,
+        silhouette_avg,
+    )
--- utils/indicator_utils.py
+++ utils/indicator_utils.py
@@ -6,96 +6,88 @@ import sys
 import pandas as pd
 from collections import Counter
 
-# def load_indicator_vectors(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask=None):
-#     if (isinstance(co2l, list) or isinstance(co2l, np.array)) and not isinstance(mask,list):
-#         #throw error if mask is not a list
-#         raise TypeError("if co2l is a list mask has to be a list, too")
-        
-#     all_vectors = []
-#     if isinstance(co2l, float):
-#         file_name = f"indicator_vector_{indicator_type}_Co2l{co2l}_n{n_nodes}.pklz"
-#         with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
-#             all_vectors = pickle.load(out)
-#         if isinstance(all_vectors,tuple):
-#             all_vectors = all_vectors[-1]
-#         if mask is not None:
-#             all_vectors = all_vectors[mask]
-#     elif isinstance(co2l, list):
-#         for i,co2 in enumerate(co2l):
-#             file_name = f"indicator_vector_{indicator_type}_Co2l{co2}_n{n_nodes}.pklz"
-#             with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
-#                 vectors_lvl = pickle.load(out)
-#             if isinstance(vectors_lvl,tuple):
-#                 vectors_lvl = vectors_lvl[-1]
-#             if mask is not None:
-#                 vectors_lvl = vectors_lvl[np.array(mask[i])]
-#             all_vectors.append(vectors_lvl)
-#         all_vectors = np.concatenate(all_vectors)
-#     else:
-#         Exception("co2l has to be float or list")
-#     return all_vectors
-
 def load_indicator_vectors(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask=None, weights=None):
     if (isinstance(co2l, list) or isinstance(co2l, np.array)) and not isinstance(mask,list):
-        #throw error if mask is not a list
         raise TypeError("if co2l is a list mask has to be a list, too")
-        
-    all_vectors = []
-    if weights is None:
-        if isinstance(co2l, float):
-            file_name = f"indicator_vector_{indicator_type}_Co2l{co2l}_n{n_nodes}.pklz"
-            with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
-                all_vectors = pickle.load(out)
-            if isinstance(all_vectors,tuple):
-                all_vectors = all_vectors[-1]
-            if mask is not None:
-                all_vectors = all_vectors[mask]
-        elif isinstance(co2l, list):
-            for i,co2 in enumerate(co2l):
-                file_name = f"indicator_vector_{indicator_type}_Co2l{co2}_n{n_nodes}.pklz"
-                with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
-                    vectors_lvl = pickle.load(out)
-                if isinstance(vectors_lvl,tuple):
-                    vectors_lvl = vectors_lvl[-1]
-                if mask is not None:
-                    vectors_lvl = vectors_lvl[np.array(mask[i])]
-                all_vectors.append(vectors_lvl)
-            all_vectors = np.concatenate(all_vectors)
-        else:
-            Exception("co2l has to be float or list")
+    
+    if not ((isinstance(co2l, list) or isinstance(co2l, np.array)) or isinstance(co2l, float)):
+        Exception("co2l has to be float or list")
+
+    if weights is None and isinstance(co2l, float):
+        all_vectors = load_indicator_vectors_unweighted_single_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask)
         return all_vectors
-    else:
-        all_weights = []
-        if isinstance(co2l, float):
-            file_name = f"indicator_vector_{indicator_type}_Co2l{co2l}_n{n_nodes}.pklz"
-            with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
-                all_vectors = pickle.load(out)
-            all_weights = [weights[x[0]] for x in all_vectors[0]]
-            all_vectors = all_vectors[-1]
-            if mask is not None:
-                all_weights = all_weights[mask]
-                all_vectors = all_vectors[mask]
-        elif isinstance(co2l, list):
-            for i,co2 in enumerate(co2l):
-                file_name = f"indicator_vector_{indicator_type}_Co2l{co2}_n{n_nodes}.pklz"
-                with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
-                    vectors_lvl = pickle.load(out)
-                weights_lvl = np.array([int(weights[x[0]]) for x in vectors_lvl[0]])
-                vectors_lvl = vectors_lvl[-1]
-                    
-                if mask is not None:
-                    vectors_lvl = vectors_lvl[np.array(mask[i])]
-                    weights_lvl = weights_lvl[np.array(mask[i])]
-                    
-                all_weights.append(weights_lvl)
-                all_vectors.append(vectors_lvl)
-            all_vectors = np.concatenate(all_vectors)
-        else:
-            Exception("co2l has to be float or list")
+
+    elif weights is None and isinstance(co2l, list):
+        all_vectors = load_indicator_vectors_unweighted_multiple_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask)
+        return all_vectors
+        
+    elif weights is not None and isinstance(co2l, float):
+        all_vectors, all_weights = load_indicator_vectors_weighted_single_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask, weights)
+        return all_vectors, all_weights
+        
+    elif weights is not None and isinstance(co2l, list):
+        all_vectors, all_weights = load_indicator_vectors_unweighted_multiple_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask, weights, all_vectors)
         return all_vectors, all_weights
 
+def load_indicator_vectors_unweighted_multiple_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask, weights, all_vectors):
+    all_weights = []
+    for i,co2 in enumerate(co2l):
+        file_name = f"indicator_vector_{indicator_type}_Co2l{co2}_n{n_nodes}.pklz"
+        with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
+            vectors_lvl = pickle.load(out)
+        weights_lvl = np.array([int(weights[x[0]]) for x in vectors_lvl[0]])
+        vectors_lvl = vectors_lvl[-1]
+                
+        if mask is not None:
+            vectors_lvl = vectors_lvl[np.array(mask[i])]
+            weights_lvl = weights_lvl[np.array(mask[i])]
+                
+        all_weights.append(weights_lvl)
+        all_vectors.append(vectors_lvl)
+    all_weights = np.concatenate(all_weights)
+    all_vectors = np.concatenate(all_vectors)
+    return all_vectors,all_weights
+
+def load_indicator_vectors_weighted_single_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask, weights):
+    all_weights = []
+    file_name = f"indicator_vector_{indicator_type}_Co2l{co2l}_n{n_nodes}.pklz"
+    with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
+        all_vectors = pickle.load(out)
+    all_weights = [weights[x[0]] for x in all_vectors[0]]
+    all_vectors = all_vectors[-1]
+    if mask is not None:
+        all_weights = all_weights[mask]
+        all_vectors = all_vectors[mask]
+    return all_vectors,all_weights
+
+def load_indicator_vectors_unweighted_multiple_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask):
+    all_vectors = []
+    for i,co2 in enumerate(co2l):
+        file_name = f"indicator_vector_{indicator_type}_Co2l{co2}_n{n_nodes}.pklz"
+        with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
+            vectors_lvl = pickle.load(out)
+        if isinstance(vectors_lvl,tuple):
+            vectors_lvl = vectors_lvl[-1]
+        if mask is not None:
+            vectors_lvl = vectors_lvl[np.array(mask[i])]
+        all_vectors.append(vectors_lvl)
+    all_vectors = np.concatenate(all_vectors)
+    return all_vectors
+    
+
+def load_indicator_vectors_unweighted_single_lvl(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask):
+    all_vectors = []
+    file_name = f"indicator_vector_{indicator_type}_Co2l{co2l}_n{n_nodes}.pklz"
+    with gzip.open(path_to_indicator_vectors + "/" + file_name, "rb") as out:
+        all_vectors = pickle.load(out)
+    if isinstance(all_vectors,tuple):
+        all_vectors = all_vectors[-1]
+    if mask is not None:
+        all_vectors = all_vectors[mask]
+    return all_vectors
+
 def create_component_indicator_vectors(n_nodes, co2l, indicator_type, path_to_indicator_vectors, mask=None):
-    """creates component wise indicator vectors from indicator vectors. Loads indicator vectors from path_to_indicator_vectors and saves component indicator vectors to path_to_indicator_vectors. The main componen is not considered a split component. Accordingly splits that contain n components are projected onto n-1 component indicator vectors. The mapping from the original indicator vector index to component indicator vector index is saved as a array.
+    """creates component wise indicator vectors from indicator vectors. Loads indicator vectors from path_to_indicator_vectors and saves component indicator vectors to path_to_indicator_vectors. The main component is not considered a split component. Accordingly splits that contain n components are projected onto n-1 component indicator vectors. The mapping from the original indicator vector index to component indicator vector index is saved as a array.
 
     Args:
         n_nodes (_type_): _description_
@@ -138,9 +130,6 @@ def create_component_indicator_vectors(n_nodes, co2l, indicator_type, path_to_in
 def calc_is_main_component_indicator_vectors(lshare_vectors, main_component_by="main_comp_max_lshare"):
     """returns a list of indicator vectors where each entry in the vectors indicates whether the corresponding node was in the main (1) component or not (0)"""
     
-    # if not isinstance(lshare_vectors, list):
-    #     lshare_vectors = [lshare_vectors]
-        
     indicator_vectors = []
     if main_component_by == "main_comp_max_lshare":
         for lshare in lshare_vectors:
