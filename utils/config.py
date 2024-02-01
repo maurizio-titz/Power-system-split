6f8c194fc1ee419508969562640cea3715aab7ae
m.titz <m.titz@fz-juelich.de>
2024-02-01 17:29:00 +0100

    (add) path_to_evaluation_results to config.py

--- utils/config.py
+++ utils/config.py
@@ -3,5 +3,6 @@
 root_path = '../' # This defaults to './', which should be the repository path
 data_path = "/media/data/system_split/data/"
 results_path = "/media/data/system_split/results/"
+path_to_evaluation_results = "/media/data/system_split/results/sclopf/evaluation_results"
 
 mattermost_url = None
