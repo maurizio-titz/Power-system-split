c163ee168a8aeb9181e59b6c4d1792e8a1e3ed2e
p.boettcher <p.boettcher@fz-juelich.de>
2024-02-23 23:03:11 +0100

    (fix) minor

--- scripts/evaluate_cascade.py
+++ scripts/evaluate_cascade.py
@@ -139,6 +139,7 @@ def evaluate_cascade(co2l: float, n_nodes: int, snet_index: int = 0,
     
     component_props_dict = dict()
     out_dict_key = 0   
+    idx_indi_vec = 0
     for timestamp, splits in tqdm(splitting_cascades_dtkeys.items(), disable=not show_progress):
         
         for ii, (init_failure, cascade) in enumerate(tqdm(splits.items(), leave=False, disable=not show_progress)):
@@ -163,7 +164,9 @@ def evaluate_cascade(co2l: float, n_nodes: int, snet_index: int = 0,
             # Append properties and vectors such that component_props.iloc[ii] refers to 
             # indicator_vectors[ii]
             if eval_indicator_vectors:
-                indicator_vectors[ii, :] = subgraph_evaluation.get_indicator_vectors_of_subgraphs(subgraphs, nx_graph)
+                indi_vec_r = subgraph_evaluation.get_indicator_vectors_of_subgraphs(subgraphs, nx_graph)
+                indicator_vectors[idx_indi_vec:idx_indi_vec + indi_vec_r.shape[0], :] = indi_vec_r
+                idx_indi_vec += indi_vec_r.shape[0]
 
     
         #component_props.to_hdf(save_path + f'component_properties_Co2L{co2l}_n{n_nodes}.h5',
