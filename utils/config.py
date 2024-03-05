35c2b880192319dc327fcb80f2646dabb7a4eeb6
m.titz <m.titz@fz-juelich.de>
2024-03-05 15:45:23 +0100

    (add) config_local.py to set root path

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-root_path = "../"  # This defaults to './', which should be the repository path
-data_path = "/media/data/system_split/data/"
-results_path = "/media/data/system_split/results/"
-path_to_indicator_vectors = "/media/data/system_split/results/sclopf/indicator_vectors/"
-path_to_clustering_results = "/media/data/system_split/results/sclopf/clustering/"
-path_to_cascade_results = "/media/data/system_split/results/sclopf/cascade_results/"
-path_to_evaluation_results = (
-    "/media/data/system_split/results/sclopf/evaluation_results/"
-)
-# path_to_pypsa_network = "/media/data/system_split/sclopf/"
-path_to_pypsa_network = "/media/data/system_split/data/European_networks_sclopf/"
+from utils.config_local import root_path
+
+data_path = root_path + "/data/"
+results_path = root_path + "/results/"
+path_to_indicator_vectors = root_path + "/results/sclopf/indicator_vectors/"
+path_to_clustering_results = root_path + "/results/sclopf/clustering/"
+path_to_cascade_results = root_path + "/results/sclopf/cascade_results/"
+path_to_evaluation_results = root_path + "/results/sclopf/evaluation_results/"
+path_to_pypsa_network = root_path + "/data/European_networks_sclopf/"
+
+mattermost_url = None
---
+++ utils/config_local.py
@@ -0,0 +1,1 @@
+root_path = "./"  # add the root path for your data and results here
