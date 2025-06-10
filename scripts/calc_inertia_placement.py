d1fa029f139fe414658cbf9f946e642ce9c4a721
mtitz <m.titz@fz-juelich.de>
2025-06-10 17:47:36 +0200

    (clean)

--- scripts/calc_inertia_placement.py
+++ scripts/calc_inertia_placement.py
@@ -1,11 +1,11 @@
-"""calculate optimal inertia placement to reduce lost load due to RoCoF. Start with large step size delta_rot_ls and decrease it by a factor of 2 in each iteration. Throws an error the placement of inertia can not mitigate any loss of load (i.e. delta_rot_ls is too small push the RoCoF over -1 for any split component).
-    """
+"""calculate optimal inertia placement to reduce lost load due to RoCoF. Start with large step size delta_rot_ls and decrease it by a factor of 2 in each iteration. Throws an error the placement of inertia can not mitigate any loss of load (i.e. delta_rot_ls is too small push the RoCoF over -1 for any split component)."""
 
 import sys
 
 sys.path.append("./")
 
 
+from utils.config import get_co2_levels
 from utils.plot_mitigation_strategies import plot_map_inertia_placement_final
 from utils.synthetic_inertia_placement import (
     run_different_parameters_for_co2lvl,
@@ -13,20 +13,26 @@ from utils.synthetic_inertia_placement import (
 )
 
 if __name__ == "__main__":
-    n_nodes = 800
-    use_sclopf = False
+    from utils.config import path_to_sclopf_data
+    import datetime
+    import os
+
+    n_nodes = 600
+    use_sclopf = True
+
+    co2l_list = get_co2_levels(n_nodes)
     # resolve_strategies = [
     #     "random",
-    #     # "concentrate",
-    #     # "hindsight",
-    #     # "hindsight_concentrate",
+    #     "concentrate",
+    #     "hindsight",
+    #     "hindsight_concentrate",
     # ]
-    resolve_strategy = "random"
+    resolve_strategy = (
+        "random"  # performance is very similar, random gives the most intuitive results
+    )
     resolve_strategies = [resolve_strategy]
     delta_rot_ls = [1000, 500, 200, 100]
-    # delta_rot_ls = [5000]
-    # delta_rot = 5000
-    for co2_lvl in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]:
+    for co2_lvl in co2l_list:
         print(f"Running for co2_lvl: {co2_lvl}")
         run_different_parameters_for_co2lvl(
             co2_lvl,
@@ -38,7 +44,7 @@ if __name__ == "__main__":
             revert_chrotE_fac=False,
             resolve_equality_method_ls=resolve_strategies,
         )
-    for co2_lvl in [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6]:
+    for co2_lvl in co2l_list:
         for delta_rot in delta_rot_ls:
             try:
                 plot_map_inertia_placement_final(
