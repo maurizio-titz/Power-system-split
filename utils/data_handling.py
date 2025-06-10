4f42bc4385eb393ac8bf48913f64e5a1b30ec439
mtitz <m.titz@fz-juelich.de>
2025-06-10 17:43:16 +0200

    (add) compare edge order pypsa vs networkx

--- utils/data_handling.py
+++ utils/data_handling.py
@@ -1,8 +1,8 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" 
-Preparation and conversion of data for system split simulation and evaluation  
+"""
+Preparation and conversion of data for system split simulation and evaluation
 """
 
 import gzip
@@ -20,9 +20,10 @@ from utils.config import (
     path_to_pypsa_network_sclopf,
     path_to_vis_results_lopf,
     path_to_vis_results_sclopf,
-    path_to_cascade_results_sclopf, 
-    path_to_cascade_results_lopf
+    path_to_cascade_results_sclopf,
+    path_to_cascade_results_lopf,
 )
+
 
 def build_networkx_graph(pypsa_network, snet_index=None):
     """Build a networkx graph from the pypsa networks"""
@@ -60,34 +61,48 @@ def build_networkx_graph(pypsa_network, snet_index=None):
     nx.set_node_attributes(F, pos, "pos")
     return F
 
-def check_order_networkx_line_order_equals_pypsa(nx_graph:nx.Graph, pypsa_network:pypsa.Network, snet=None):
+
+def check_order_networkx_line_order_equals_pypsa(
+    nx_graph: nx.Graph, pypsa_network: pypsa.Network, snet=None
+):
     """
     Check if the edges in the networkx graph are sorted in ascending order
     """
     if snet:
-        lines_subnet = pypsa_network.lines[pypsa_network.lines.sub_network.astype(int) == snet]
+        lines_subnet = pypsa_network.lines[
+            pypsa_network.lines.sub_network.astype(int) == snet
+        ]
     else:
         lines_subnet = pypsa_network.lines
 
     # return np.array(list(nx.get_edge_attributes(nx_graph, "orientation").values())) == np.array(list(list(zip(lines_subnet.bus0.astype(str), lines_subnet.bus1.astype(str)))))
 
-    for i, (u, v) in enumerate(nx.get_edge_attributes(nx_graph, "orientation").values()):
-        if (u,v) != (lines_subnet.bus0[i], lines_subnet.bus1[i]):
-            raise (RuntimeError(f"Edge {i} in networkx ({u}, {v}) does not match pypsa ({lines_subnet.bus0[i]}, {lines_subnet.bus1[i]})"))
+    for i, (u, v) in enumerate(
+        nx.get_edge_attributes(nx_graph, "orientation").values()
+    ):
+        if (u, v) != (lines_subnet.bus0[i], lines_subnet.bus1[i]):
+            raise (
+                RuntimeError(
+                    f"Edge {i} in networkx ({u}, {v}) does not match pypsa ({lines_subnet.bus0[i]}, {lines_subnet.bus1[i]})"
+                )
+            )
     return True
 
     # zip(nx.get_edge_attributes(nx_graph, "orientation").values(), list(zip(pypsa_network.lines.bus0, pypsa_network.lines.bus1)))
 
-def check_if_edges_sorted(nx_graph:nx.Graph):
+
+def check_if_edges_sorted(nx_graph: nx.Graph):
     """
     Check if the edges in the networkx graph are sorted in ascending order
     """
-    
-    for i, (u, v) in enumerate(nx.get_edge_attributes(nx_graph, "orientation").values()
-):
+
+    for i, (u, v) in enumerate(
+        nx.get_edge_attributes(nx_graph, "orientation").values()
+    ):
         if u > v:
             raise (RuntimeError(f"Edge {i} ({u}, {v}) is not sorted"))
     return True
+
 
 def construct_incidencematrix_from_orientation(Graph, return_np_array=True):
     """Construct incidence matrix for a graph with edge keyword orientation specifying the edge order
@@ -207,19 +222,21 @@ def load_pypsa_network_from_path(path_to_pypsa_network: str, use_sclopf: bool):
 
     return network
 
+
 def load_cascades(co2lvl, n_nodes, use_sclopf: bool = True):
     if use_sclopf:
         path_to_cascade_results = path_to_cascade_results_sclopf
     else:
         path_to_cascade_results = path_to_cascade_results_lopf
-        
+
     full_path_to_cascades = (
-            path_to_cascade_results + f"system_splits_Co2L{co2lvl}_n{n_nodes}.pklz"
-        )
+        path_to_cascade_results + f"system_splits_Co2L{co2lvl}_n{n_nodes}.pklz"
+    )
     with gzip.open(full_path_to_cascades, "rb") as fh_casc:
         splitting_cascades = pickle.load(fh_casc)
-        
+
     return splitting_cascades
+
 
 def get_subgraphs_from_edges(edge_indices, nx_graph):
     """Generate subgraphs that result from removing the edges.
@@ -233,13 +250,13 @@ def get_subgraphs_from_edges(edge_indices, nx_graph):
     """
 
     check_if_edges_sorted(nx_graph)
-    
+
     nx_edges = matrix_indices_to_nx_edges(edge_indices, nx_graph)
 
     F = nx_graph.copy()
     F.remove_edges_from(nx_edges)
     subgraphs = list((F.subgraph(c).copy() for c in nx.connected_components(F)))
-    
+
     for subgraph in subgraphs:
         check_if_edges_sorted(subgraph)
 
@@ -292,6 +309,21 @@ def get_matrices_from_nx_graph(nx_graph):
     )
 
     return I_m, B_d, num_parallels, line_limits
+
+
+def get_adjacency_matrix_from_nx_graph(nx_graph):
+    """returns the adjacency matrix of the nx graph"""
+    I_m, B_d, num_parallels, line_limits = data_handling.get_matrices_from_nx_graph(
+        nx_graph
+    )
+    I_m = np.absolute(I_m)
+    adjacency_matrix = I_m @ I_m.T
+    # Remove self-loops for sparse matrices
+    if hasattr(adjacency_matrix, "setdiag"):
+        adjacency_matrix.setdiag(0)
+    else:
+        np.fill_diagonal(adjacency_matrix, 0)  # remove self-loops
+    return adjacency_matrix
 
 
 def load_split_props(n_nodes: int, co2l=None, use_sclopf: bool = True):
