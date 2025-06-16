6cefb64aab5f194f23055af20da731fed65885a3
mtitz <m.titz@fz-juelich.de>
2025-06-16 14:40:52 +0200

    (fix) unintentionally rounding co2lvl in file names

--- utils/synthetic_inertia_placement.py
+++ utils/synthetic_inertia_placement.py
@@ -1,7 +1,7 @@
 #!usr/bin/env python
 # -*- coding: utf-8 -*-
 
-"""Place synthetic inertia to mitigate dangerous system splits 
+"""Place synthetic inertia to mitigate dangerous system splits
 and lost load. This is done via adding rot energy to individual nodes
 iteratively (i.e., greedily to the one mitigating the most lost load)."""
 
@@ -511,13 +511,13 @@ def run_specific_co2lvl_n_size(
 
     fpath_component_in = (
         path_to_evaluation_results
-        + f"component_properties_Co2L{co2_lvl:.1f}_n{nn_nodes}.h5"
+        + f"component_properties_Co2L{co2_lvl}_n{nn_nodes}.h5"
     )
     component_df = pd.read_hdf(fpath_component_in, key="df")
 
     fpath_indicator_vec_in = (
         path_to_evaluation_results
-        + f"component_indicator_vectors_Co2L{co2_lvl:.1f}_n{nn_nodes}.pklz"
+        + f"component_indicator_vectors_Co2L{co2_lvl}_n{nn_nodes}.pklz"
     )
     with gzip.open(fpath_indicator_vec_in, "rb") as fh_in:
         indicator_vec_arr = pickle.load(fh_in)
