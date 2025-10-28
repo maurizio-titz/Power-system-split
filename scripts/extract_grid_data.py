98edd0d6965dbfe8bcdf3f94fd122c7831b16822
mtitz <m.titz@fz-juelich.de>
2025-10-28 10:46:11 +0100

    minor

--- scripts/extract_grid_data.py
+++ scripts/extract_grid_data.py
@@ -21,7 +21,9 @@ for co2l in co2ls:
     network = data_handling.load_pypsa_network(
         n_nodes=600, co2lvl=co2l, use_sclopf=False, lopt=use_extensions
     )
-    nx_graph = data_handling.build_networkx_graph(network, snet_index=snet_index)
+    nx_graph = data_handling.build_networkx_graph(
+        network, snet_index=snet_index, update_lines=use_extensions
+    )
     data_handling.save_networkx_graph(nx_graph, snet_index=snet_index, co2lvl=co2l)
     I_m, B_d, num_parallels, line_limits = data_handling.get_matrices_from_nx_graph(
         nx_graph
