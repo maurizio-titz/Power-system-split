c29635cd8f07366a43665fcd58e3d4d31ed57073
mtitz <m.titz@fz-juelich.de>
2026-02-08 11:52:53 +0100

    (add) clustering plots

---
+++ scripts/plots/plot_clusters.py
@@ -0,0 +1,51 @@
+# %%
+import numpy as np
+import sys
+
+sys.path.append("../")
+sys.path.append("./")
+from utils.data_handling import get_co2_levels
+from utils.clustering.blackout_clustering_class import Clustering
+from utils.clustering.distance_metrics import geometric_mean
+from hdbscan import HDBSCAN
+
+from scripts.cluster_blackouts import (
+    distance_metric_kwargs,
+    clustering_params,
+)
+
+n_nodes = 600
+co2l_iter = list(get_co2_levels(n_nodes))
+cl = Clustering(
+    n_nodes,
+    co2l_iter,
+    indicator_type="rocof",
+    transformation="blackout",
+    blackout_size_threshold=0.1,
+    distance_metric_kwargs=distance_metric_kwargs,
+    clustering_params=clustering_params,
+    distance_matrix_dtype=np.float16,
+)
+cl.load_data()
+cl.transform_vectors()
+cl.filter_data()
+cl.create_clustering_results_index()
+# %%
+cl.plot_cluster_multiple(
+    # relative_score_threshold=0.1,
+    n_best=4,
+    average_over_classes=False,
+    algorithm="agg",
+    sort_by="frequency",
+)
+cl.plot_cluster_multiple(
+    n_best=4,
+    # relative_score_threshold=0.1,
+    average_over_classes=False,
+    algorithm="agg",
+)
+# cl.plot_cluster_multiple(
+#     n_best=4,
+#     average_over_classes=False,
+#     algorithm="agg",
+# )
