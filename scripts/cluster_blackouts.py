e966fcff68c432ee1f1bc719073bb3a45f9fc6f4
mtitz <m.titz@fz-juelich.de>
2026-02-16 13:49:02 +0100

    (clean up)

--- scripts/cluster_blackouts.py
+++ scripts/cluster_blackouts.py
@@ -1,16 +1,8 @@
 # %%
-# %load_ext autoreload
-# %autoreload 2
-from datetime import datetime
-from functools import partial
-import gzip
-import os
-import pickle
 import sys
 from scipy.stats import uniform
 from scipy.stats import loguniform
 import numpy as np
-from scipy import sparse
 
 sys.path.append("../")
 sys.path.append("./")
@@ -70,12 +62,6 @@ distance_metric_kwargs = {
     "metrics": [
         {
             "name": "hamming",
-            # "preprocessing": {
-            #     "method": "low_weight_boundary_field",
-            #     "target": "weights",  # Use as weights
-            #     "alpha": 0.4,
-            #     "steps": 3,
-            # },
             "n_jobs": n_jobs_distance,
         },
         {
@@ -110,7 +96,7 @@ clustering_params = {
         },
         "calc_silhouette": True,
     },
-    ## HDBSCAN didnÄt work well.
+    ## HDBSCAN didn't work well.
     # "hdbscan": {
     #     "n_iter": 128,
     #     "n_jobs": n_jobs_clustering,
@@ -128,22 +114,32 @@ clustering_params = {
 
 # %%
 if __name__ == "__main__":
-    # parse arguments
     import argparse
 
     parser = argparse.ArgumentParser(description="Cluster blackouts")
     parser.add_argument(
         "--testing", action="store_true", help="Run in testing mode", default=False
     )
+    parser.add_argument(
+        "--co2l", nargs="+", type=float, help="CO2 levels to cluster", default=None
+    )
+    parser.add_argument(
+        "--threshold", type=float, help="Blackout size threshold", default=0.1
+    )
+
     args = parser.parse_args()
     testing = args.testing
+    threshold = args.threshold
 
     n_nodes = 600
     if testing:
         co2l_list = [0.6, 0.5]
         random_subsample_size = 0.001
     else:
-        co2l_list = list(get_co2_levels(n_nodes))
+        if args.co2l is not None:
+            co2l_list = args.co2l
+        else:
+            co2l_list = list(get_co2_levels(n_nodes))
         random_subsample_size = None
 
     co2l_clustering = [co2l_list]
@@ -156,7 +152,7 @@ if __name__ == "__main__":
             co2l_iter,
             indicator_type="rocof",
             transformation="blackout",
-            blackout_size_threshold=0.99,
+            blackout_size_threshold=threshold,
             distance_metric_kwargs=distance_metric_kwargs,
             clustering_params=clustering_params,
             distance_matrix_dtype=np.float16,
@@ -164,12 +160,10 @@ if __name__ == "__main__":
             clustering_worker_func=_fit_clustering_worker_script,  # Use script-level worker for pickling
         )
         cl.load_data()
-        cl.filter_data()
         cl.transform_vectors()
-        # ############### comment out if you only want to plots >>>>
-        # cl.get_distance_matrix()
-        # cl.fit_clusters()
-        # ############### comment out if you only want to plots <<<<
+        cl.filter_data()
+        cl.get_distance_matrix()
+        cl.fit_clusters()
         cl.create_clustering_results_index()
         cl.plot_cluster_multiple(
             # relative_score_threshold=0.1,
@@ -183,9 +177,5 @@ if __name__ == "__main__":
             # relative_score_threshold=0.1,
             average_over_classes=False,
             algorithm="agg",
+            sort_by="accumulative_lost_load",
         )
-        # cl.plot_cluster_multiple(
-        #     n_best=4,
-        #     average_over_classes=False,
-        #     algorithm="agg",
-        # )
