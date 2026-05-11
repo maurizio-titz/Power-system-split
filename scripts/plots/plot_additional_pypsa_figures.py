dd0aa17cf41a8622a732ba0b608074cfddebfb75
Lange, Jan <j.lange@fz-juelich.de>
2026-05-11 17:11:16 +0200

    added investment supl. plot

--- scripts/plots/plot_additional_pypsa_figures.py
+++ scripts/plots/plot_additional_pypsa_figures.py
@@ -10,7 +10,7 @@ import pickle
 
 from utils.data_handling import \
     (load_pypsa_network, build_networkx_graph, 
-     get_co2_levels)
+     get_co2_levels, get_actual_co2_level)
 
 from utils.plot_style import *
 setup_matplotlib_style()
@@ -26,7 +26,7 @@ from loguru import logger
 
 from matplotlib import pyplot as plt
 import matplotlib.colors as mplcolors
-
+import pandas as pd
 
 def histogram_lineloading_accross_co2lvls(n_nodes: int = 600, calc_again: bool = False,
                                           condition_on_split: bool = False,
@@ -178,4 +178,190 @@ def graph_with_num_parallel(co2lvl: float = .6, n_nodes: int = 600,
     return
 
 
+
+def plot_investments_capital_costs(n_nodes,agg_solar = True, agg_gas = True):
+    co2ls = get_co2_levels(n_nodes=n_nodes)
+    networks = {
+        c:   load_pypsa_network(co2lvl=c, n_nodes=n_nodes)
+        for c in co2ls
+    }
+
+    n_ref = networks[co2ls[0]]
+
+    colors_gens = n_ref.carriers.color
+    nice_names = n_ref.carriers.nice_name
+    short_nice_names = {"battery": "Battery", "hydro": "Hydro", "PHS": "Pumped hydro"}
+    for name, nicename in short_nice_names.items():
+        nice_names[name] = nicename
+
+    opex = {}
+    capex = {}
+    storage_capex = {}
+
+    for co2, n in networks.items():
+
+        # Operational costs
+        gen_dispatch = n.generators_t.p.sum()
+
+        gen_opex = (
+            gen_dispatch
+            * n.generators.marginal_cost
+        ) #€/MWh
+
+        gen_opex = gen_opex.groupby(
+            n.generators.carrier
+        ).sum()
+
+        opex[co2] = gen_opex
+       # Generator investment costs
+
+        gen_capex = (
+            n.generators.p_nom_opt
+            * n.generators.capital_cost
+        )
+
+        gen_capex = gen_capex.groupby(
+            n.generators.carrier
+        ).sum()
+       # Storage investment costs
+
+        if len(n.storage_units) > 0:
+            sto_cap = (
+                n.storage_units.p_nom_opt
+                * n.storage_units.capital_cost
+            )
+
+            sto_cap = sto_cap.groupby(
+                n.storage_units.carrier
+            ).sum()
+        else:
+            sto_cap = pd.Series(dtype=float)
+
+        capex[co2] = gen_capex
+        storage_capex[co2] = sto_cap
+
+
+    # CONVERT TO DATAFRAMES
+    opex_df = pd.DataFrame(opex).fillna(0).T
+    capex_df = pd.DataFrame(capex).fillna(0).T
+    storage_df = pd.DataFrame(storage_capex).fillna(0).T
+
+
+    opex_df = opex_df.drop(columns=["geothermal", "art_load","load"])
+    capex_df = capex_df.drop(columns=["geothermal", "art_load","load"])
+
+    if agg_solar == True:
+        opex_df_solar = (opex_df['CCGT'] + opex_df['OCGT']).copy()
+        opex_df = opex_df.drop(columns=["CCGT", "OCGT"])
+        opex_df['gas'] = opex_df_solar
+        capex_df_solar = (capex_df['solar'] + capex_df['solar-hsat']).copy()
+        capex_df = capex_df.drop(columns=["solar", "solar-hsat"])
+        capex_df['solar'] = capex_df_solar
+
+    if agg_gas == True:
+        opex_df_solar = (opex_df['solar'] + opex_df['solar-hsat']).copy()
+        opex_df = opex_df.drop(columns=["solar", "solar-hsat"])
+        opex_df['solar'] = opex_df_solar
+        capex_df_gas = (capex_df['CCGT'] + capex_df['OCGT']).copy()
+        capex_df = capex_df.drop(columns=["CCGT", "OCGT"])
+        capex_df['gas'] = capex_df_gas
+        colors_gens['gas'] =   "#cc0099"
+        nice_names['gas'] = 'Gas'
+
+    colors_gens['H2'] = "green"
+    colors_gens['battery'] = "tab:olive"
+
+    opex_df_offwind = (opex_df['offwind-dc'] + opex_df['offwind-ac']+ opex_df['offwind-float']).copy()
+    opex_df = opex_df.drop(columns=["offwind-dc", "offwind-ac",'offwind-float'])
+    opex_df['offwind'] = opex_df_offwind
+
+    capex_df_offwind = (capex_df['offwind-dc'] + capex_df['offwind-ac'] + capex_df['offwind-float']).copy()
+    capex_df = capex_df.drop(columns=["offwind-dc", "offwind-ac",'offwind-float'])
+    capex_df['offwind'] = capex_df_offwind
+
+
+    colors_gens['offwind'] =  colors_gens['offwind-dc']
+    nice_names['offwind'] = 'Offshore Wind'
+
+    # PLOT
+    fname_fig = "supl_costs_per_co2"
+    fig, axes = plt.subplots(
+        3, 1,
+        figsize=(10, 12),
+        sharex=True
+    )
+
+    # ----------------------------
+    # OPEX
+    # ----------------------------
+
+    opex_df.plot(
+        kind="bar",
+        stacked=True,
+        ax=axes[0],
+        color=colors_gens[opex_df.columns]
+
+    )
+
+    axes[0].set_ylabel(r"Cost [€/a]", fontsize=AXIS_LABEL_FONTSIZE)
+
+    # CAPEX
+    capex_df.plot(
+        kind="bar",
+        stacked=True,
+        ax=axes[1],
+        color=colors_gens[capex_df.columns],
+        legend=False
+
+    )
+
+    axes[1].set_ylabel(r"Cost [€/a]", fontsize=AXIS_LABEL_FONTSIZE)
+
+    # STORAGE CAPEX
+    storage_df.plot(
+        kind="bar",
+        stacked=True,
+        ax=axes[2],
+        color=colors_gens[storage_df.columns],
+    )
+
+    axes[2].set_ylabel(r"Cost [€/a]", fontsize=AXIS_LABEL_FONTSIZE)
+    axes[2].set_xlabel(r"CO$_2$ level [\% of 1990]", fontsize=AXIS_LABEL_FONTSIZE)
+
+
+    handles, labels = axes[0].get_legend_handles_labels()
+
+    new_labels = [nice_names.get(l, l).capitalize() for l in labels.copy()]
+
+    axes[0].legend(handles, new_labels,
+                    loc="upper left",
+                    ncol=2,
+                    fontsize=LEGEND_FONTSIZE,
+    )
+
+    handles, labels = axes[2].get_legend_handles_labels()
+
+    new_labels = [nice_names.get(l, l).capitalize() for l in labels.copy()]
+
+    axes[2].legend(handles, new_labels, 
+                    loc="upper right",
+                    ncol=2,
+                    fontsize=LEGEND_FONTSIZE,
+    )
+
+
+    co2_order = np.array(sorted(co2ls))
+    x_labels = [f"{get_actual_co2_level(c, percent=True):g}%" for c in co2_order]
+    axes[2].set_xticklabels(x_labels, rotation=0)
+
+    for idx, ax in enumerate(axes):
+        add_panel_label(ax, idx, x_offset=-0.-0.03)
+        ax.tick_params(axis="both", which="both", labelsize=TICK_LABEL_FONTSIZE)
+
+    fig.tight_layout()
+    plt.show()
+
+    save_figure(fig, fname_fig, path_to_figures_sclopf)
+
+    return
 
